4e87ea4f355b3d53f2001747e41be97552697f92
youi1586zxc <3788020380@qq.com>
2025-08-29 22:10:03 +0800

    更改站点域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://quiyun.com/",
+  site: "https://quiyu.cn/",
   base: "/",
   trailingSlash: "always",
   integrations: [
